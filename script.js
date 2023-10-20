d02a8f7ca61582ef5fbad93fd7d815fa6baf7df4
mrmeepmeep <myachou42@gmail.com>
2023-10-20 23:15:04 +0200

    Added UI for Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,66 +1,43 @@
-let playerScore = 0;
-let computerScore = 0;
-const game = () => {
-for (let i = 0; i < 5; i++) {
-    randomNumber = Math.floor(Math.random() * 3) + 1;
+// DOM Manipulation
+const rockBtn = document.querySelector('#rockBtn')
+const paperBtn = document.querySelector('#paperBtn')
+const scissorsBtn = document.querySelector('#scissorsBtn')
 
-function getComputerChoice(){
-   
-    if (randomNumber === 1) {
-        return "ROCK";
-    }  
-    else if (randomNumber == 2){
-        return "PAPER";
+
+//Playerchoice
+rockBtn.addEventListener('click', function(){
+    return 'rock'
+})
+
+paperBtn.addEventListener('click', function(){
+    return 'paper'
+})
+
+scissorsBtn.addEventListener('click', function(){
+    return 'scissors'
+})
+
+
+
+
+
+
+
+function getRandomChoice(){
+    let random = Math.floor(Math.random() *3 ) +1
+        console.log(random)
+    if (random === 1){
+        return 'rock'
     }
-    else {
-        return "SCISSORS";
+    if (random === 2){
+        return "paper"
+    }
+    if (random === 3){
+        return "scissors"
     }
 }
-function playRound(playerSelection, computerSelection){
-if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-    playerScore++;
-    return "You win!";
-} else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
-    playerScore++;
-    return "You win!";
-}  else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-    playerScore++;
-    return "You win!";
-} else if (playerSelection === computerSelection) {
-        return "Tie!";
-} else {
-    computerScore++;
-    return "You lose!";
-}
-}
 
-
-player = prompt("pick", '');
-const playerSelection = player.toUpperCase();
-const computerSelection = getComputerChoice();
-const playRoundResult = playRound(playerSelection, computerSelection);
-console.log(playRound(playerSelection,computerSelection));
-
-console.log(`Player pick: ${playerSelection}`);
-console.log(`Computer pick: ${computerSelection}`);
-console.log(`You: ${playerScore}`);
-console.log(`Computer ${computerScore}`);
-
-}
-}
-
-game();
-
-//function score(){
-//  if (playRound) === "You win!"){
-//      playerScore++
-// } else if (playRound() === "You lose!"){
-//      computerScore++
-//  } else {
-//    return playerScore+= 0
-//  }
-//  }
-
-
-
+getRandomChoice()
+playerSelection = this.textContent()
+computerSelection = getRandomChoice()
 
